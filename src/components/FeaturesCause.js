b5e82ba7b86512756dae831ba871355fd05a71ac
kaderabdul <kaderabdul7980@gmail.com>
2022-02-21 00:39:11 +0600

    finish cause part

--- src/components/FeaturesCause.js
+++ src/components/FeaturesCause.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import Title from '../elements/Title';
 import '../styles/FeaturesCause.css';
+import cause1 from '../images/a8.jpg';
 
 const FeaturesCause = () => {
     return (
         <div>
             <Title></Title>
+            <div className="trending-cause">
+                <div className="cause">
+                    <img src={cause1} alt="" />
+                    <div className="author">
+                        <span>Health</span>
+                        <span>By Abdul K.</span>
+                    </div>
+                    <h2 class="causetitle">People Health Response And Village Mans</h2>
+                    <div className="goal">
+                        <span><b>15%</b>Raised</span>
+                        <span><b>7000</b>Goal</span>
+                    </div>
+                </div>
+                <div className="cause">
+                    <img src={cause1} alt="" />
+                    <div className="author">
+                        <span>Health</span>
+                        <span>By Abdul K.</span>
+                    </div>
+                    <h3>People Health Response And Village Mans</h3>
+                    <div className="goal">
+                        <span><b>15%</b>Raised</span>
+                        <span><b>7000</b>Goal</span>
+                    </div>
+                </div>
+                <div className="cause">
+                    <img src={cause1} alt="" />
+                    <div className="author">
+                        <span>Health</span>
+                        <span>By Abdul K.</span>
+                    </div>
+                    <h3>People Health Response And Village Mans</h3>
+                    <div className="goal">
+                        <span><b>15%</b>Raised</span>
+                        <span><b>7000</b>Goal</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/TrendingCause.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TrendingCause = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TrendingCause;
