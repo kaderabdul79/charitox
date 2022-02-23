43a4638642a6aab2848835dea6c241d363194477
kaderabdul <kaderabdul7980@gmail.com>
2022-02-23 23:59:26 +0600

    login form has been finish now

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Login.css';
 
 const Login = () => {
@@ -9,8 +10,10 @@ const Login = () => {
                 <form class="form">
                     <div className='email'><input type="email" name="email" value="" placeholder='enter email'></input></div>
                     <div className='password'><input type="password" name="password" value="" placeholder='enter password'></input></div>
+                    <Link to="/forgot">Forgot Password</Link>
                     <div className="submit"><input type="submit" value="Login" /></div>
                 </form>
+                Have no Account ? <Link to="/signup">Signup Now</Link>
             </div>
         </div>
     );
