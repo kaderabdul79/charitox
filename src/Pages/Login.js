94bdeab843b14a3419dc0c5be83025166e7e6d02
kaderabdul <kaderabdul7980@gmail.com>
2022-02-24 13:16:45 +0600

    Get the currently signed-in user

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../hooks/useFirebase';
 import '../styles/Login.css';
 
 const Login = () => {
+    const {usingGoogleSignin, usingGithubSignin} = useFirebase()
+    //console.log(usingGoogleSignin)
+    const GoogleSignin = () => {
+        usingGoogleSignin()
+    }
+    const GithubSignin = () => {
+        usingGithubSignin()
+    }
+    const handleSignin = (e) => {
+        e.preventDefault();
+    }
     return (
         <div className='formlogin'>
             <div className="loginform">
                 <div className="login">Login Form</div>
-                <form class="form">
-                    <div className='email'><input type="email" name="email" value="" placeholder='enter email'></input></div>
-                    <div className='password'><input type="password" name="password" value="" placeholder='enter password'></input></div>
+                <form className="form">
+                    <div className='email'><input type="email" name="email" placeholder='enter email'></input></div>
+                    <div className='password'><input type="password" name="password" placeholder='enter password'></input></div>
                     <Link to="/forgot">Forgot Password</Link>
-                    <div className="submit"><input type="submit" value="Login" /></div>
+                    <div className="submit"><input onClick={handleSignin} type="submit" name="Login" /></div>
                 </form>
-                Have no Account ? <Link to="/signup">Signup Now</Link>
+                    Have no Account ? <Link to="/signup">Signup Now</Link>
                 <div className="provider">
-                    <button className='google'><Link to="/login">Connect with Google</Link></button>
-                    <button className='github'><Link to="/login">Connect with Github</Link></button>
+                    <button className='google' onClick={GoogleSignin} >Connect with Google</button>
+                    <button className='github' onClick={GithubSignin}>Connect with Github</button>
                 </div>
             </div>
         </div>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import React, { useEffect, useState } from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged } from "firebase/auth";
+import initializeFirebase from '../Firebase/firebase.init';
 
+initializeFirebase();
 
 const useFirebase = () => {
     const [user,setUser] = useState({})
@@ -8,7 +10,8 @@ const useFirebase = () => {
 
     const auth = getAuth();
     const googleprovider = new GoogleAuthProvider();
-    
+    const githubprovider = new GithubAuthProvider();
+
     //Authenticate Using Google Sign-In
     const usingGoogleSignin = () => {
         signInWithPopup(auth, googleprovider)
@@ -22,8 +25,36 @@ const useFirebase = () => {
 
     }
 
+    //Authenticate Using Github Sign-In
+    const usingGithubSignin = () => {
+        signInWithPopup(auth, githubprovider)
+        .then((result) => {
+            const user = result.user;
+            setUser(user)
+            console.log(user)
+        }).catch((error) => {
+            const email = error.email;
+            setUser(error.message)
+            console.log(email)
+        });
+
+    }
+
+    // Get the currently signed-in user
+    useEffect(()=>{
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+              setUser(user)
+            } else {
+              // User is signed out
+              setUser({})
+            }
+          });
+          return () => unsubscribed;
+    },[])
+
     return {
-        usingGoogleSignin
+        usingGoogleSignin,usingGithubSignin
     };
 };
 
