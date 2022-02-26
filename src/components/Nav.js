19974d3aaa65437c5514afd7f9d008b518523e1e
kaderabdul <kaderabdul7980@gmail.com>
2022-02-27 00:15:23 +0600

    designed the nav button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,19 +10,19 @@ const Nav = () => {
             <div className="header-nav">
             <div className="logo"><h2>charitox</h2></div>
             {/* list item start */}
-            <div className="list-item">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/events">events</Link></li>
-                <li><Link to="/news">News</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                {
-                    user.email ? <li><Link onClick={logOut}>Logout</Link></li>
-                    : <li><Link to="/login">Login</Link></li>
-                }
-                <li><Link to="/donate" className='btn-donate'>Donate Us</Link></li>
-            </ul>
+            <div className="nav-list-item">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/events">events</Link></li>
+                    <li><Link to="/news">News</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    {
+                        user.email ? <li><Link onClick={logOut}>Logout</Link></li>
+                        : <li><Link to="/login">Login</Link></li>
+                    }
+                    <li><Link to="/donate" className='btn-donate'>Donate Us</Link></li>
+                </ul>
             </div>
              {/* list item end */}
              </div>  
