c8fc0a370899260b406a262dd328b888b3c93fc9
kaderabdul <kaderabdul7980@gmail.com>
2022-03-10 11:17:04 +0600

    design the news page

--- src/Pages/News.js
+++ src/Pages/News.js
@@ -2,30 +2,80 @@ import React from 'react';
 import '../styles/News.css';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
+import Title from '../elements/Title';
 
 const News = () => {
     return (
-        <div className='news-area'>
+        <div className=''>
             <Nav></Nav>
+            <Title toptitle="Update News" bigtitle="Put Your Hand with Us"></Title>
+            <div className="news-area">
             <div className="news-grid">
                 <div className="news">
                     <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
-                    <h2>NATO’s inadequate response to Russia’s invasion of Ukraine</h2>
-                    <p>People take shelter in a subway station in Kyiv, Ukraine, on February 24. If world leaders cannot come up with the right solutions, they should at least refrain from making irresponsible and meaningless statements. This is the least the people of Ukraine deserve right now. PHOTO: REUTERS</p>
-                    <div className="btn">read more</div>
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
                 </div>
                 <div className="news">
                     <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
-                    <h2>NATO’s inadequate response to Russia’s invasion of Ukraine</h2>
-                    <p>People take shelter in a subway station in Kyiv, Ukraine, on February 24. If world leaders cannot come up with the right solutions, they should at least refrain from making irresponsible and meaningless statements. This is the least the people of Ukraine deserve right now. PHOTO: REUTERS</p>
-                    <div className="btn">read more</div>
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
                 </div>
                 <div className="news">
                     <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
-                    <h2>NATO’s inadequate response to Russia’s invasion of Ukraine</h2>
-                    <p>People take shelter in a subway station in Kyiv, Ukraine, on February 24. If world leaders cannot come up with the right solutions, they should at least refrain from making irresponsible and meaningless statements. This is the least the people of Ukraine deserve right now. PHOTO: REUTERS</p>
-                    <div className="btn">read more</div>
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
                 </div>
+                <div className="news">
+                    <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
+                </div>
+                <div className="news">
+                    <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
+                </div>
+                <div className="news">
+                    <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
+                </div>
+                <div className="news">
+                    <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
+                </div>
+                <div className="news">
+                    <img src="https://images.thedailystar.net/sites/default/files/styles/big_202/public/images/2022/02/24/download_2.jpg?itok=UNQ6RPnA&timestamp=1645723954" alt="" />
+                    <div className="article">
+                    <h3>NATO’s inadequate response to Russia’s invasion of Ukraine</h3>
+                    <article>People take shelter in a subway station....</article>
+                    <span className="btn">read more</span>
+                    </div>
+                </div>
+            </div>
             </div>
             <Footer></Footer>
         </div>
